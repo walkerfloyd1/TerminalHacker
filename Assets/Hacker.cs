9c9f7e3ce1012c196f75a24b6e2576a451cc8df6
walkerfloyd1 <walkerkeatley@yahoo.com>
2019-07-09 12:00:27 -0700

    basics

---
+++ Assets/Hacker.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hacker : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        ShowMainMenu();
+    }
+
+    void ShowMainMenu ()
+    {
+
+        Terminal.ClearScreen();
+        string greeting = "What's Up Bitch";
+        Terminal.WriteLine(greeting);
+        Terminal.WriteLine("Choose!");
+        Terminal.WriteLine("1 - Blockbusters (easy)");
+        Terminal.WriteLine("2 - Oscar Films (medium)");
+        Terminal.WriteLine("3 - Indie Films (hard)");
+        Terminal.WriteLine("Enter your selection:");
+    }
+        // Update is called once per frame
+        void Update()
+    {
+        
+    }
+}
