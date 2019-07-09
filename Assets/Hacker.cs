0c35c6d3f2af583d797a4f41b50e9f5292421284
walkerfloyd1 <walkerkeatley@yahoo.com>
2019-07-09 12:48:31 -0700

    16-MessagesAreSpecialFunctions

--- Assets/Hacker.cs
+++ Assets/Hacker.cs
@@ -7,20 +7,23 @@ public class Hacker : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        ShowMainMenu();
+        ShowMainMenu("What's Up Bitch");
     }
 
-    void ShowMainMenu ()
+    void ShowMainMenu (string greeting)
     {
 
         Terminal.ClearScreen();
-        string greeting = "What's Up Bitch";
         Terminal.WriteLine(greeting);
         Terminal.WriteLine("Choose!");
         Terminal.WriteLine("1 - Blockbusters (easy)");
         Terminal.WriteLine("2 - Oscar Films (medium)");
         Terminal.WriteLine("3 - Indie Films (hard)");
         Terminal.WriteLine("Enter your selection:");
+    }
+    void OnUserInput (string input)
+    {
+        print(input);
     }
         // Update is called once per frame
         void Update()
